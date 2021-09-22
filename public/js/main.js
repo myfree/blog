cf9c67a32e4323a1ed612286e16aa2e37a4f1a05
Nov8nana <shuxhan@163.com>
2021-09-22 16:53:21 +0800

    🍉 smile face

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,5 @@ $(function () {
   $('iframe').wrap('<p class="iframe"></p>')
 })
 
-console.log('Powered by Nov8nana ⚡ date 20210915 https://shuxhan.com/')
-console.log('Your smile is like the sweetest bite in watermelon. 🍉 The person I like is called Bohui. ❤\n')
+console.log('Powered by hugo and Author by Nov8nana ⚡ date 2020.06.14 https://shuxhan.com/')
+console.log('Your smile is like the sweetest bite in watermelon. 🍉 The person I like is called hb. ❤\n')
