5b04affd103d7962e9b911a793b3296703c16686
Nov8nana <shuxhan@163.com>
2021-10-12 13:52:34 +0800

    Organize the code

---
+++ themes/nov/static/js/main.js
@@ -0,0 +1,21 @@
+$(function () {
+  $('iframe').wrap('<p class="iframe"></p>')
+
+  $(".top-btn").click(function () {
+    $("html,body").animate({
+      scrollTop: '0px'
+    }, 500);
+  });
+
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() >= 200) {
+      $('.top-btn').addClass('top-btn-show')
+    } else {
+      $('.top-btn').removeClass('top-btn-show')
+    }
+  });
+
+})
+
+console.log('Powered by hugo and Author by Nov8nana ⚡ date 2020.06.14 https://github.com/Nov8nana')
+console.log('Your smile is like the sweetest bite in watermelon. 🍉 The person I like is called xx. ❤\n')
