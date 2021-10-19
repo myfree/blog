66cd07ce8d178f68bb4f4a731d589542943eb3c0
Nov8nana <shuxhan@163.com>
2021-10-19 11:37:30 +0800

    发布了新的博客

--- README.md
+++ README.md
@@ -3,15 +3,18 @@
 * 项目源码仓库: `main`
 * 静态文件路径: `/public`
 
-Bolg: [https://imzsh.com](https://imzsh.com)
+我的个人博客
 
-Comment Repo: [https://github.com/Nov8nana/blog-comment-Vssue](https://github.com/Nov8nana/blog-comment-Vssue)
+* 项目地址: [https://github.com/Nov8nana/hugo-blog](https://github.com/Nov8nana/hugo-blog)
+* 在线阅读 Bolg: [https://imzsh.com](https://imzsh.com)
 
-### Use
+
+### Introduce
 
 1. Hugo是由Go语言实现的静态网站生成器。简单、易用、高效、易扩展、快速部署。
 2. 如果想使用博客，可以直接 fork 本仓库，将 `content` 以及下面 `posts` 内的文件清空，写入自己的文章即可。
 3. 本地安装 go 程序，具体方法自行搜索。
+4. 主题是个人开发的，没有上架官方主题商店，需要的可以直接下载仓库使用。
 
 ```shell
 ## 本地运行 
@@ -23,12 +26,13 @@ hugo -D
 
 ```
 
-### 配置
+### Question
 
-一些主要功能的配置方法已经写在配置文件里，如果有其他配置看不懂的可以在 Issues 留言。
+如果有任何疑问或错误，欢迎在 issues 进行提问或给予修正意见。
 
+如果喜欢或对你有所帮助，欢迎 Star，对作者是一种鼓励和推进 😀
 
-### 额外的一些说明协议
+### Directions
 
 本仓库并不是一个真正意义上的开源项目，下载仓库代码及使用后果概不负责。
 
--- public/js/main.js
+++ public/js/main.js
@@ -19,31 +19,16 @@ $(function () {
   $('.nav-menu').click(function () {
     if (i == 1) {
       $(this).toggleClass('nav-menu-open');
-      $('.aside-header .nav ul').toggleClass('nav-menu-ul');
+      $('.header nav ul').toggleClass('nav-menu-ul')
     } else {
       $('.nav-menu').removeClass('nav-menu-open');
-      $('.aside-header .nav ul').removeClass('nav-menu-ul');
+      $('.header nav ul').removeClass('nav-menu-ul')
     }
   });
 
-  if (window.location.pathname == "/") {
-    $('.aside-header .nav li:nth-child(1) a').addClass('active')
-  }
 
 
 })
 
 console.log('Powered by hugo and Author by Nov8nana ⚡ date 2020.06.14 https://github.com/Nov8nana')
 console.log('Your smile is like the sweetest bite in watermelon. 🍉 The person I like is called xx. ❤\n')
-
-var myNav = document.getElementById("nav").getElementsByTagName("a");
-var myURL = document.location.href;
-for (var i = 1; i < myNav.length; i++) {
-  var links = myNav[i].getAttribute("href");
-  var myURL = document.location.href;
-  if (myURL.indexOf(links) != -1) {
-    myNav[i].className = "active";
-    myNav[0].className = "";
-  }
-}
-
