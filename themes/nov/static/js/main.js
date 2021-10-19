3db4ab38ebcf6d607bbc9cb0485bda20a09d520b
Nov8nana <shuxhan@163.com>
2021-10-19 15:39:08 +0800

    😍 Use jq to improve the reading experience

--- themes/nov/static/js/main.js
+++ themes/nov/static/js/main.js
@@ -26,7 +26,12 @@ $(function () {
     }
   });
 
-
+  $('#TableOfContents a').click(function () {
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+    return false;
+  });
 
 })
 
